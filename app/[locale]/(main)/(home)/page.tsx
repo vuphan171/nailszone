689a37dee384e47841ed85ea75d84809855dffab
Vu Phan <vu.pham.levincigroup@gmail.com>
2026-05-03 19:22:10 +0700

    refactor: remove getHomePage API call and notFound handling; uncomment HeroBanner component for improved home page layout

--- app/[locale]/(main)/(home)/page.tsx
+++ app/[locale]/(main)/(home)/page.tsx
@@ -1,11 +1,8 @@
 import { Suspense } from "react"
 
-import { notFound } from "next/navigation"
-
 import { Locale } from "next-intl"
 import { setRequestLocale } from "next-intl/server"
 
-import { getHomePage } from "@/lib/api/home"
 import { PreloadQuery } from "@/lib/graphql/apollo-client"
 import { GET_HOMEPAGE_QUERY } from "@/lib/graphql/queries/home"
 
@@ -20,16 +17,10 @@ const Page = async ({ params }: { params: Promise<{ locale: string }> }) => {
 
   setRequestLocale(locale as Locale)
 
-  const data = await getHomePage(1)
-
-  if (!data) {
-    return notFound()
-  }
-
   return (
     <>
       <div className="mt-4 flex shrink grow flex-col gap-y-4 bg-surface px-8 pt-16">
-        {/* <HeroBanner /> */}
+        <HeroBanner />
         <FeedComposer />
         <div className="mx-auto w-full flex max-w-3xl flex-col gap-y-4">
           <PreloadQuery
